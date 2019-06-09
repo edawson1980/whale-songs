314e98f44e3e8bc545c17fda5f33f11375d47ee3
Erin <erinedawson@gmail.com>
2019-06-08 19:20:16 -0600

    write for loop using input phrase

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 let input = 'pizza party'; //this is the phrase for translation
 
 const vowels = ['a', 'e', 'i', 'o', 'u']; //these are the vowels to be used in translation
+
+for(let i = 0; i < input.length; i++){ //this loop runs through each character in input
+
+};
