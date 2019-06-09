c54b853ad380c3b67868e6c51a5f1ea5597efbc3
Erin <erinedawson@gmail.com>
2019-06-08 19:29:09 -0600

    compare vowels and push matches to results array

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ let input = 'pizza party'; //this is the phrase for translation
 
 const vowels = ['a', 'e', 'i', 'o', 'u']; //these are the vowels to be used in translation
 
+let resultArray = [];
+
 for(let i = 0; i < input.length; i++){ //this loop runs through each character in input
   for(let v = 0; v < vowels.length; v++){ //this loops runs through vowels array each time the input loop runs
-
+    if(input[i] === vowels[v]){ //to access a certain letter in a string, cite it like an array
+      resultArray.push(vowels[v]); //add any vowels in input to the results array
+    }
   }
 };
