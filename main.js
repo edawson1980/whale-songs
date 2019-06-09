661f04a84500a57620b62fc4b0757373d3ca06b8
Erin <erinedawson@gmail.com>
2019-06-08 19:23:42 -0600

    add nested for loop

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ let input = 'pizza party'; //this is the phrase for translation
 const vowels = ['a', 'e', 'i', 'o', 'u']; //these are the vowels to be used in translation
 
 for(let i = 0; i < input.length; i++){ //this loop runs through each character in input
+  for(let v = 0; v < vowels.length; v++){ //this loops runs through vowels array each time the input loop runs
 
+  }
 };
